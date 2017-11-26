11b81e1bff45b79d60c49e716cfe1e9433e065f9
Weston Ruter <weston@xwp.co>
2017-11-26 14:00:09 -0800

    Fix jscs in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,6 @@ module.exports = function( grunt ) {
 	// Load tasks
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	// grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-shell' );
@@ -127,7 +126,6 @@ module.exports = function( grunt ) {
 	grunt.registerTask( 'default', [
 		'jshint',
 		'uglify'
-		// 'cssmin'
 	] );
 
 	grunt.registerTask( 'readme', [
